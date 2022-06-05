ffc6586548f24c9fa455fc4a985b81b946354080
Thugft <106910325+Thugft@users.noreply.github.com>
2022-06-05 17:46:42 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'hè lố bạn của tôi!',
+    introDesc: `sao bạn kì lạ thê nhỉ
+  
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    title: 'bạn có bị khùng không '
+    btnYes: 'có',
+    btnNo: 'Không ',
+    question:'hỏi lại nè bạn có bị thần kinh không ',
+    
+    reply: 'có mình bị lâu r',
+    mess: 'tao biết mà ',
+    messDesc: 'ba mẹ mày có bik mày bị không',
+    btnAccept: 'không',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
